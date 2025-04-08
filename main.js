4fc146755060d81e60bd2018ea40b8608318ff69
yasminfranco <yasfranco98@gmail.com>
2025-04-08 15:05:53 -0300

    Conectando dom

---
+++ main.js
@@ -0,0 +1,14 @@
+const $ = (elem) => document.querySelector(elem);
+const $$ = (elem) => document.querySelector(elem);
+
+const $inputSearch = $("#input-search");
+const $selectType = $("#search-type");
+const $selectOrder = $("#select-order");
+const $buttonSearch = $("#button-search");
+const $divContainerResults = $("#container-results");
+const $buttonFirstPag = $("#button-first-pag");
+const $buttonPreviousPag = $("#button-previous-pag");
+const $buttonNextPag =  $("#button-next-pag");
+const $buttonLastPag = $("#button-last-pag");
+const $divEpisodeDetail = $("#episode-detail");
+const $divCharacterDetail = $("#characters-detail");
