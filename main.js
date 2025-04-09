92485398f79eaa98cd8b2b237217937dd0f2871d
yasminfranco <yasfranco98@gmail.com>
2025-04-09 14:29:44 -0300

    Función para mostrar los personajes en pantalla

--- main.js
+++ main.js
@@ -12,3 +12,45 @@ const $buttonNextPag =  $("#button-next-pag");
 const $buttonLastPag = $("#button-last-pag");
 const $divEpisodeDetail = $("#episode-detail");
 const $divCharacterDetail = $("#characters-detail");
+
+// const showData = (array,type) => {
+//     $divContainerResults.innerHTML = "";
+
+//     for (const item of array) {
+//         $divContainerResults.innerHTML += `<img class="mx-4 h-5 w-5" src="https://rickandmortyapi.com/api/${type}"/>` 
+//     }
+// }
+
+const showData = (arrayPersonajes) =>{
+    $divContainerResults.innerHTML = "";
+
+    for (const personaje of arrayPersonajes) {
+        $divContainerResults.innerHTML += `<div class="flex flex-col items-center m-6 bg-white rounded-2x1 shadow-md overflow-hidden w-72 hover:scale-105 transition-transform">
+        <img class="w-full h-60 object-cover" src="${personaje.image}" alt="${personaje.name}" />
+        <div class="p-4 space-y-2">
+        <h3 class="text-xl font-bold text-slate-800">${personaje.name}</h3>
+        </div>
+        </div>` 
+    }
+}
+
+let dataCharacters = [];
+
+const getData = async () => {
+    try {
+        const { data } = await axios.get("https://rickandmortyapi.com/api/character")
+        dataCharacters = data.results
+        console.log("funcion get data",dataCharacters)
+
+        showData(dataCharacters);
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+window.onload = async () => {
+    await getData()
+}
+// https://rickandmortyapi.com/api/episode
+// https://rickandmortyapi.com/api/character
